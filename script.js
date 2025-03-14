d7b05741726426baaca8b6c68decf17bcb06605b
das45rt <145970892+das45rt@users.noreply.github.com>
2025-03-14 13:07:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ document.getElementById('chatButton').addEventListener('click', function() {
     const userInput = document.getElementById('userInput').value.toLowerCase();
     const botResponseElement = document.getElementById('botResponse');
 
-    if (userInput === "привет") {
-        botResponseElement.innerText = "Привет!";
-    } else {
-        botResponseElement.innerText = "Я не понимаю. Пожалуйста, напишите 'привет'.";
+    // Простой "искусственный интеллект" для ответов на вопросы
+    let botResponse = "";
+
+    switch(userInput) {
+        case "привет":
+            botResponse = "Привет, как дела?";
+            break;
+        case "как дела":
+            botResponse = "У меня все хорошо, спасибо!";
+            break;
+        case "как жизнь":
+            botResponse = "Жизнь прекрасна! А как у вас?";
+            break;
+        case "сколько месяцев в году":
+            botResponse = "В году 12 месяцев.";
+            break;
+        case "что ты умеешь":
+            botResponse = "Я могу отвечать на простые вопросы!";
+            break;
+        default:
+            botResponse = "Извините, я не знаю, как на это ответить.";
+            break;
     }
+
+    botResponseElement.innerText = botResponse;
 });
