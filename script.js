77e5e90543c1821adf5039280079d84b2e7c93fc
das45rt <145970892+das45rt@users.noreply.github.com>
2025-03-14 13:00:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.getElementById('chatButton').addEventListener('click', function() {
-    // Ответ бота
-    const botResponse = "Привет!";
-    document.getElementById('botResponse').innerText = botResponse;
+    const userInput = document.getElementById('userInput').value.toLowerCase();
+    const botResponseElement = document.getElementById('botResponse');
+
+    if (userInput === "привет") {
+        botResponseElement.innerText = "Привет!";
+    } else {
+        botResponseElement.innerText = "Я не понимаю. Пожалуйста, напишите 'привет'.";
+    }
 });
